19b4a96a525d57fc09f3ebf103863e21383a9d9c
Shivity2003 <91420995+Shivity2003@users.noreply.github.com>
2023-07-25 23:42:22 +0530

    increment or decrement by hooks

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import React, { useState } from 'react';
+import ToDoLists from './ToDoLists';
+
+const App =()  =>{
+};
+return(
+    <>
+    <div className="main_div">
+        <div className="center_div">
+            <br/>
+            <h1>
+                To Do List
+            </h1>
+            <br/>
+            
+            <button onClick={listOfItems}> +  </button>  
+            <ol>
+               
+              
+            </ol> </div>
+    </div>
+    </> 
+        )
+       
+;
+    export default App;
